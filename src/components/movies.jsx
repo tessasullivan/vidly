0f1d06032255c884612b14aa31fd57467e3c1978
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-07-03 13:20:01 -0700

    Pagination UI added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1
   };
 
   handleDelete = movie => {
@@ -15,13 +18,18 @@ class Movies extends Component {
   handleLike = movie => {
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
-    movies[index] = {...movies[index]};
+    movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
-    this.setState({movies});
+    this.setState({ movies });
+  };
+
+  handlePageChange = page => {
+    this.setState({currentPage: page});
   };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage} = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -47,7 +55,10 @@ class Movies extends Component {
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
-                  <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
                 </td>
                 <td>
                   <button
@@ -61,6 +72,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
