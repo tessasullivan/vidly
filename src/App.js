216b5c08273e817ef7db7d377a67579dab43eb32
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-07-04 15:46:49 -0700

    Added Routing, NavBar, and Movie Details components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import './App.css';
-import Movies from './components/movies';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import NavBar from "./components/navbar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import "./App.css";
 
-function App() {
-  return (
-    <main className="container">
-      <Movies/>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}/>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found"/>
+        </Switch>
+      </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
